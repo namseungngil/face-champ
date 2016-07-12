7458ad6e14a1ec65972eb9c8cfc5d4743389ee0d
namseungil <sinam@minamichanBluetooth-PAN.local>
2016-07-12 20:23:00 +0900

    set unity cloud build

---
+++ Unity/Assets/Face/Scripts/Intro/Controller/IntroController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using Hellgate;
+
+public class IntroController : FaceSceneController
+{
+    public static void Main ()
+    {
+    }
+
+    public override void OnSet (object data)
+    {
+        base.OnSet (data);
+
+        FaceSceneManager.Instance.Wait (2.0f, delegate() {
+            FaceSceneManager.Instance.Screen ("Main");
+        });
+    }
+}
---
+++ Unity/Assets/Face/Scripts/Main/Controller/MainController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using Hellgate;
+
+public class MainController : FaceSceneController
+{
+    public static void Main ()
+    {
+        
+    }
+
+    public override void OnSet (object data)
+    {
+        base.OnSet (data);
+    }
+}
